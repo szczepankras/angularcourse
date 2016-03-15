90aca9867e3393810e0af2a8d7e82884c5aa825d
Bartek <bslipek@gmail.com>
2016-03-15 18:01:31 +0000

    Adds basic webpack config

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+import angular from 'angular';
+
+
+const ngModule = angular.module('app', []);
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    context: __dirname + '/app',
+    entry: './index.js',
+    output: {
+      path: __dirname + '/app',
+      filename: 'bundle.js'
+    },
+    
+    module: {
+      loaders: [
+        {test: /\.js$/, loader: 'babel', exclude: /node_modules/},
+        {test: /\.css$/, loader: 'style!css'}
+      ]
+    }
+       
+}
